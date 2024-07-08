e153df796b7473f82ce80ceeed111268cc7cd8c7
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2024-07-08 14:59:58 +0700

    update option menu

---
+++ OptionMenu/app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_demo, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
